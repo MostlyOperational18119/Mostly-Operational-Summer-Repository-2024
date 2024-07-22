b422600dc62e786614206e77a8f36f7b6dc84165
BetheBob1 <BetheBob082208@gmail.com>
2024-07-22 16:38:24 -0700

    ufbrsifsef

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleopWithOdometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleopWithOdometry.java
@@ -2,9 +2,13 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.firstinspires.ftc.teamcode.Autonomous.poseStorage;
 import org.firstinspires.ftc.teamcode.DriveMethods;
@@ -23,7 +27,7 @@ public class TeleopWithOdometry extends DriveMethods {
         DcMotor motorFR = hardwareMap.get(DcMotor.class, "motorFR");
         DcMotor motorBR = hardwareMap.get(DcMotor.class, "motorBR");
         motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
-        double speedDiv = 2.0;
+        double speedDiv = 1.0;
 
         telemetry.addLine("Status");
         telemetry.addLine("Initialized");
@@ -31,6 +35,8 @@ public class TeleopWithOdometry extends DriveMethods {
 
         waitForStart();
         while(opModeIsActive()) {
+            Pose2d myPose = drive.getPoseEstimate();
+
             double leftX = gamepad1.left_stick_x;
             double leftY = -gamepad1.left_stick_y;
             double rightX = gamepad1.right_stick_x;
@@ -41,7 +47,15 @@ public class TeleopWithOdometry extends DriveMethods {
             motorFR.setPower((leftY - leftX - rightX) / speedDiv);
 
             drive.update();
-            Pose2d myPose = drive.getPoseEstimate();
+
+            if (gamepad1.a){
+                Trajectory myTrajectory = drive.trajectoryBuilder(myPose).
+                        lineToConstantHeading(new Vector2d(0.0,0.0))
+                        .build();
+
+                drive.followTrajectory(myTrajectory);
+            }
+
 
             telemetry.addData("x", myPose.getX());
             telemetry.addData("y", myPose.getY());
